4b935eb1561d9df76552ea6f5eb228b168b41334
VipraN9 <99337998+VipraN9@users.noreply.github.com>
2024-12-27 13:44:49 +0530

    Update auth.middleware.ts

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,20 +1,16 @@
 import { Request, Response, NextFunction } from "express";
 import { User } from "../models/user.model";
-/**
- * Middleware to simulate user authentication and role-based access control.
- */
 
 const user = {
     id: "12345",
     username: "adminUser",
-    role: "admin", // This can be "admin" or "user"
+    role: "admin", 
   };
 
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
   try {
     req.user = user;
 
-    // Proceed to the next middleware or route handler
     next();
   } catch (error) {
     console.error("Authentication error:", error);
@@ -22,15 +18,13 @@ export const authMiddleware = (req: Request, res: Response, next: NextFunction) 
   }
 };
 
-/**
- * Middleware to check for admin role.
- */
+
 export const adminMiddleware = (req: Request, res: Response, next: NextFunction): void => {
     try {
       // Check if the user exists and is an admin
       if (!req.user || req.user.role !== "admin") {
         res.status(403).json({ message: "Forbidden: Admins only" });
-        return; // Ensure no further code runs after sending a response
+        return;
       }
   
       // If the user is an admin, proceed to the next middleware/handler
